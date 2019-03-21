287c316c9ef24125e42aca3930866122ed5ee4b3
Martino Pinna <37085112+marty-p@users.noreply.github.com>
2019-03-21 10:01:12 +0100

    Create sys.py

---
+++ lezione03/sys.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 09:59:36 2019
+
+@author: Studente
+"""
+
+import sys
+print(sys.path)
+sys.path.insert(0, '.')
+print(sys.path)
+
+import __builtin__ as bt
+dir(bt)
