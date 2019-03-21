1d31917bf25aee84020d440a512bdd7bc2fd5e02
Martino Pinna <37085112+marty-p@users.noreply.github.com>
2019-03-21 09:50:46 +0100

    Create load_fib.py

---
+++ lezione03/load_fib.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 09:46:15 2019
+
+@author: Studente
+"""
+
+import fibonacci
+#from lezione03 import fibonacci
+#from lezione03 import fibonacci as fib
+fibonacci.__name__
