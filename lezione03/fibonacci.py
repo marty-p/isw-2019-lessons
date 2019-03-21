9ad995811a8efeb948e3fbee507afb997cd583b7
Martino Pinna <37085112+marty-p@users.noreply.github.com>
2019-03-21 09:49:17 +0100

    Create fibonacci.py

---
+++ lezione03/fibonacci.py
@@ -0,0 +1,33 @@
+### FUNZIONI
+def fibonacci(n):
+    """Questa funzione calcola la serie di fibonacci fino al
+       valore n inserito.
+    ===========================
+
+    Params:
+        n: estremo della serie di Fibonacci (int):
+    Returns:
+        None
+    """
+    a,b=0,1
+    while a < n:
+        print(a, end=' ')
+        c = b+a
+        b,a=a,c
+
+if __name__ = '__main__':
+    res = fibonacci(15) # the def has no return, res is None
+    print(res)
+
+def fibonacci_2(n):
+    a,b=0,1
+    fib=[]
+    while a<n:
+        fib.append(a)
+        c=b+a
+        b,a=a,c
+    return fib
+
+if __name__ = '__main__':
+    res_2 = fibonacci_2(n=15)
+    print(res_2)
