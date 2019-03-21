badb55c56225512aaad1a36055413f97ba75b378
Martino Pinna <37085112+marty-p@users.noreply.github.com>
2019-03-21 09:19:07 +0100

    Update lezione03.py

--- lezione03.py
+++ lezione03.py
@@ -2,7 +2,7 @@ def finonacci(n):
     """Questa funzione calcola la serie di fibonacci fino al
        valore n inserito.
     ===========================
-    
+
     Params:
         n: estremo della serie di Fibonacci (int):
     Returns:
@@ -13,4 +13,5 @@ def finonacci(n):
         print(a, end=' ')
         c = b+a
         b,a=a,c
+
 res = fibonacci(15)
