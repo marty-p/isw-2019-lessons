978d7678827610909068010a60f548ae42b69b36
Martino Pinna <37085112+marty-p@users.noreply.github.com>
2019-03-21 10:22:31 +0100

    Create mathdatetime.py

---
+++ lezione03/mathdatetime.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 10:20:10 2019
+
+@author: Studente
+"""
+
+import math
+math.cos(math.pi/4)
+
+math.sqrt(81)
+
+import random
+random.choice(["apple","pear","banana","watermelon"])
+
+random_numbers = random.sample(range(100), 10)
+print(random_numbers)
+
+random.random()
+
