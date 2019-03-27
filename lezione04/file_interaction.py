da7631260209eaf1054117d2c08ba021e8e47771
Martino Pinna <37085112+marty-p@users.noreply.github.com>
2019-03-27 09:47:01 +0100

    Add files via upload

--- lezione04/file_interaction.py
+++ lezione04/file_interaction.py
@@ -27,3 +27,60 @@ with open("textfile.txt", "r") as f:
 with open("textfile.txt", "r") as f:
     string = f.read()
     print(string)
+
+
+
+
+
+
+
+
+
+# esercizio: creare un file, metterci quello che vogliamo con \n
+# l'obbiettivo è quello di leggere il file creato e creare una lista di liste, dove ogni sotto lista contiene una linea del file
+
+# v1
+c1=["aaaaaaa\n", "bbbbbbb\n", "ccccccc\n", "ddddddd\n"]
+with open("esercizio4file.txt", "w") as f1:
+    f1.writelines(c1)
+with open("esercizio4file.txt") as f1:
+    c2=f1.readlines()
+    print(c2)
+# v2
+c1=["aaaaaaa\n", "bbbbbbb\n", "ccccccc\n", "ddddddd\n"]
+c2=[]
+with open("esercizio4file.txt", "w") as f1:
+    f1.writelines(c1)
+with open("esercizio4file.txt") as f1:
+    for line in f1.readlines():
+        l=[]
+        l.append(line)
+        c2.append(l)
+print(c2)
+
+# v3
+c1=["aaaaaaa\n", "bbbbbbb\n", "ccccccc\n", "ddddddd\n"]
+c2=[]
+with open("esercizio4file.txt", "w") as f1:
+    f1.writelines(c1)
+with open("esercizio4file.txt") as f1:
+    c2 = [line for line in f1.readlines()]
+print(c2)
+
+# v4
+c1=["aaaaaaa\n", "bbbbbbb\n", "ccccccc\n", "ddddddd\n"]
+c2=[]
+with open("esercizio4file.txt", "w") as f1:
+    f1.writelines(c1)
+with open("esercizio4file.txt") as f1:
+    c2 = [[line] for line in f1.readlines()]
+print(c2)
+
+# v5
+c1=["aaaaaaa\n", "bbbbbbb\n", "ccccccc\n", "ddddddd\n"]
+c2=[]
+with open("esercizio4file.txt", "w+") as f1:
+    f1.writelines(c1)
+    f1.seek(0)
+    c2 = [[line] for line in f1.readlines()]
+print(c2)
