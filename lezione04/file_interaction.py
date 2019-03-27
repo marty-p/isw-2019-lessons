6fd08a264f7136402ace98f6a6ef0df057dbc55a
Martino Pinna <37085112+marty-p@users.noreply.github.com>
2019-03-27 09:35:43 +0100

    Add files via upload

---
+++ lezione04/file_interaction.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 27 09:24:42 2019
+
+@author: Studente
+"""
+
+#create writable file
+with open("textfile.txt", "w") as f:
+    f.write("hello world\n")
+
+# error f is already closed
+iterable=["this is a test\n", "to write new lines\n", "new line"]
+f.writelines(iterable)
+
+# without the with clause
+f=open("textfile.txt", "a")
+f.writelines(iterable)
+f.close()
+
+# read content as a list
+with open("textfile.txt", "r") as f:
+    contents = f.readlines()
+
+# read content as single str
+with open("textfile.txt", "r") as f:
+    string = f.read()
