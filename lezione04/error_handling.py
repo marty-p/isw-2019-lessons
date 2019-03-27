cb33a63414f44740f5d758fb1fad9065a163e0b1
Martino Pinna <37085112+marty-p@users.noreply.github.com>
2019-03-27 10:44:00 +0100

    Add files via upload

--- lezione04/error_handling.py
+++ lezione04/error_handling.py
@@ -71,4 +71,41 @@ while True:
 # o al III quadrante. i punti già inseriti non devono essere considerati.
 # l'utente deve inserire numero non stringhe. il punto deve essere realmente un
 # punto del piano cartesiano (controllo sulla lunghezza dell'input inserito)
-#        
+#
+piano_len = (50, 50, -50, -50)
+
+def get_input_point():
+    res = (0,0)
+    while True:
+        i = input("please enter a point: ")
+        # skip empty lines
+        if not i.strip():
+            continue
+        # check for empty spaces
+        r1=i.split(" ")
+        if not len(r1)==2: # must have x y
+            r1=i.split(",") # if not split for ,
+            if not len(r1)==2: #and recheck
+                print("no valid input")
+                continue # then repeat
+        try:
+            res=[int(i) for i in r1]
+        except ValueError:
+            print("no valid input")
+
+        # check if within plane
+        if res[0] > piano_len[0]:
+            print("x > region I x")
+            continue
+        elif res[1] > piano_len[1]:
+            print("y > region I y")
+            continue
+        elif res[0] < piano_len[2]:
+            print("x < region III x")
+            continue
+        elif res[1] < piano_len[3]:
+            print("y < region III y")
+            continue
+        break
+    return res
+get_input_point()
