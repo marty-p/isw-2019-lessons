0554780352d4de3ceddd60af0ce2a136d8da0611
Martino Pinna <37085112+marty-p@users.noreply.github.com>
2019-04-04 09:27:22 +0200

    Add files via upload

--- lezione05/class_intro.py
+++ lezione05/class_intro.py
@@ -17,4 +17,21 @@ print(foo_instance.i)
 foo_instance.i = 10
 print(foo_instance.i)
 
+foo_instance.printer('ciao')
 
+class Superheroes():
+    kind = 'superheroes'
+    def __init__(self, hero):
+        self.hero = hero
+
+inst_1 = Superheroes('Superman')
+inst_2 = Superheroes('Batman')
+inst_3 = Superheroes('Spiderman')
+
+print(inst_1.kind)
+print(inst_2.kind)
+print(inst_3.kind)
+
+print(inst_1.hero)
+print(inst_2.hero)
+print(inst_3.hero)
