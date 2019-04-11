54fbcf85f0d5bab23fa2f89f0400da8087299dc4
Martino Pinna <37085112+marty-p@users.noreply.github.com>
2019-04-11 10:18:35 +0200

    Add files via upload

---
+++ lezione06/esercizio/esercizio.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 11 10:14:57 2019
+
+@author: Studente
+"""
+
+#1. costruisci una classe che prende il percorso del file csv
+#2. poi avete un metodo opendataset che apre il file scaricato
+#oltre ad aprire il csv deve verificare che ci sia 'Close'
+#convertirla in numpy array
+#3. poi creare un altro metodo
+#che aggiunge in maniera random dei np.nan nel numpy array
+
+class EsReader:
+    def __init__(filepath):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    filepath = r"C:\Users\Studente\Desktop\isw\lezione06\BTC-USD.csv"
+    esReader = EsReader(filepath)
+    
