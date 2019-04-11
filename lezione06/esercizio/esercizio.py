ebb2bd1bdab4a6a8b7b25824be4c42248a3c86d4
Martino Pinna <37085112+marty-p@users.noreply.github.com>
2019-04-11 10:54:27 +0200

    Add files via upload

--- lezione06/esercizio/esercizio.py
+++ lezione06/esercizio/esercizio.py
@@ -4,6 +4,7 @@ Created on Thu Apr 11 10:14:57 2019
 
 @author: Studente
 """
+#https://finance.yahoo.com/quote/BTC-USD/history?period1=1428703200&period2=1554933600&interval=1d&filter=history&frequency=1d
 
 #1. costruisci una classe che prende il percorso del file csv
 #2. poi avete un metodo opendataset che apre il file scaricato
@@ -11,6 +12,8 @@ Created on Thu Apr 11 10:14:57 2019
 #convertirla in numpy array
 #3. poi creare un altro metodo
 #che aggiunge in maniera random dei np.nan nel numpy array
+#4. mettere i nan e poi toglierli mettendoci la media dei 6 valori sopra
+
 
 import pandas as pd
 import numpy as np
@@ -33,16 +36,24 @@ class EsReader:
         return False
         
     def addRandom(self):
-        randomSet = np.array(random.sample(range(100), 20))
+        #randomSet = np.array(random.sample(range(100), 20))
+        randomSet = np.array([np.nan]*40)
         self.closeSet = np.append(self.closeSet, randomSet)
 
+    def calcNan(self, r=6):
+        for i,v in enumerate(self.closeSet):
+            if np.isnan(self.closeSet[i]):
+                #print(self.closeSet[i-r:i])
+                self.closeSet[i]=np.average(self.closeSet[i-r:i])
 
 
 if __name__ == '__main__':
-    filename = r"C:\Users\Studente\Desktop\isw\lezione06\BTC-USD.csv"
+    filename = r"C:\Users\Studente\Desktop\isw\lezione06\esercizio\BTC-USD.csv"
     esReader = EsReader(filename)
     esReader.readData()
-    print(esReader.dataset.columns)
-    print(esReader.closeSet)
+    #print(esReader.dataset.columns)
+    #print(esReader.closeSet)
     esReader.addRandom()
-    print(esReader.closeSet)
+    #print(esReader.closeSet)
+    esReader.calcNan()
+    a=esReader.closeSet
